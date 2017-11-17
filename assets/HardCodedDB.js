443d10e927c235233a7e35c01e3096972637ad4b
Eric Soler <e.j.soler1@gmail.com>
2017-11-16 22:04:06 -0800

    Added Card For LoadoutView

--- assets/HardCodedDB.js
+++ assets/HardCodedDB.js
@@ -39,14 +39,16 @@ const EmptyItem =
         guid: 'A321D67F-32CC-44BB-B0F7-5F14DF6AD216',
         displayName: 'EmptyItemName',
         descriptiveReason: 'Some blah blah blah reason about why you need this item',
-        imgResourceName: 'some path to an image',
+        imageUri: 'some path to an image',
         uri: 'https://eil.com/shop/moreinfo.asp?catalogid=466977' 
     }
 
 const EmptyLoadout = {
         guid: '694840D5-88F4-4250-94FF-A820377CF9FB',
         category: 'testCategory',
+        description: 'Some blah blah blah reason about why you need this pack',
         displayName: 'EmptyLoadout',
+        imageUri: 'someUri',
         entryList: [EmptyItem]
 
     }
@@ -57,7 +59,7 @@ const Weed =
     guid: '9D22EBE6-FA8C-4E29-BF4D-963FEF5F9931',
     displayName: 'Weed',
     descriptiveReason: 'Some blah blah blah reason about why you need this item',
-    imgResourceName: 'some path to an image',
+    imageUri: 'some path to an image',
     uri: 'https://eil.com/shop/moreinfo.asp?catalogid=466977' 
 }
 
@@ -66,7 +68,7 @@ const MjPipe =
     guid: '17F9CDFF-B577-4852-976C-DA26A35E1E74',
     displayName: 'Pipe',
     descriptiveReason: 'Some blah blah blah reason about why you need this item',
-    imgResourceName: 'some path to an image',
+    imageUri: 'some path to an image',
     uri: 'https://eil.com/shop/moreinfo.asp?catalogid=466977' 
 }
 
@@ -75,7 +77,7 @@ const Lighter =
     guid: 'A321D67F-32CC-44BB-B0F7-5F14DF6AD216',
     displayName: 'Lighter',
     descriptiveReason: 'Some blah blah blah reason about why you need this item',
-    imgResourceName: 'some path to an image',
+    imageUri: 'some path to an image',
     uri: 'https://eil.com/shop/moreinfo.asp?catalogid=466977' 
 }
 
@@ -84,14 +86,16 @@ const Gum =
     guid: '16A404C0-A93D-407D-8735-D8EECF35A331',
     displayName: 'Gum',
     descriptiveReason: 'Some blah blah blah reason about why you need this item',
-    imgResourceName: 'some path to an image',
+    imageUri: 'some path to an image',
     uri: 'https://eil.com/shop/moreinfo.asp?catalogid=466977' 
 }
 
 const SmokingWeedLoadout = {
     guid: 'F2D7C3AD-CF1D-483E-AC90-7F56EE608837',
     category: 'Drugs',
+    description: 'Some blah blah blah reason about why you need this pack',
     displayName: 'Smoking Weed Loadout',
+    imageUri: 'someUri',
     entryList: [Weed, MjPipe, Lighter, Gum]
 }
 
@@ -102,7 +106,7 @@ const Daypack =
     guid: '6840D8FC-B2C8-4DDE-82C2-CB9E28B5788B',
     displayName: 'Daypack',
     descriptiveReason: 'Some blah blah blah reason about why you need this item',
-    imgResourceName: 'some path to an image',
+    imageUri: 'some path to an image',
     uri: 'https://eil.com/shop/moreinfo.asp?catalogid=466977' 
 }
 
@@ -111,14 +115,16 @@ const HikingBoots =
     guid: '6C816D14-33A0-45D1-8F56-32218EA21F03',
     displayName: 'Hiking Boots',
     descriptiveReason: 'Some blah blah blah reason about why you need this item',
-    imgResourceName: 'some path to an image',
+    imageUri: 'some path to an image',
     uri: 'https://eil.com/shop/moreinfo.asp?catalogid=466977' 
 }
 
 const HikingLoadout = {
     guid: '40118D61-18E1-48F9-9A81-EE3E91396554',
     category: 'testCategory',
+    description: 'Some blah blah blah reason about why you need this pack',
     displayName: 'Hiking Essentials',
+    imageUri: 'someUri',
     entryList: [Daypack, HikingBoots]
 
 }
