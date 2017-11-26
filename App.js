55c8a3c97d228f0bcc9e6f1b06cbfc64f8687557
Christopher DuBois <chdubois@cisco.com>
2017-11-26 15:45:34 -0800

    feat(meteor): set up basic meteor connection

--- App.js
+++ App.js
@@ -2,8 +2,22 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 // import SearchHardCodedDBScreen from './screens/SearchHardCodedDBScreen';
 import MyNavigator from './navigator/MyNavigator';
+import Meteor from 'react-native-meteor';
+
+const SERVER_URL = 'ws://10.0.0.30:3000/websocket';
 
 export default class App extends React.Component {
+  // TODO remove
+  componentWillMount() {
+    Meteor.connect(SERVER_URL);
+  }
+  // TODO remove
+  componentDidMount() {
+    Meteor.call('trendingList.get', undefined, (err, res) => {
+      console.log('trendingList.get', err, res);
+    });
+  }
+
   render() {
     return (
       <MyNavigator style={styles.container}/>
